83425a81eb805261b2def38c531ff9bfc0fe6817
Iván Ros <ivanros92@gmail.com>
2023-04-01 22:45:48 +0200

    PodcastCard IT test completed

---
+++ tests/podcast-card.test.tsx
@@ -0,0 +1,21 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { PodcastCard } from '../src/components/podcast-card/podcast-card';
+
+const mockProps = {
+  image: 'http://example.com/podcastimage.png',
+  title: 'Jest Podcast',
+  author: 'Jester Testingman',
+};
+
+describe('PodcastCard', () => {
+  it('checks that card elements are displayed correctly', async () => {
+    render(<PodcastCard {...mockProps} />);
+
+    expect(
+      screen.queryByAltText(`podcast-card-${mockProps.title}-${mockProps.author}`),
+    ).toBeInTheDocument();
+    expect(screen.getByLabelText('Podcast title')).toHaveTextContent(mockProps.title);
+    expect(screen.getByLabelText('Podcast author')).toHaveTextContent(mockProps.author);
+  });
+});
